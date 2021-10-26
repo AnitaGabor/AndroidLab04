090581823fffe3e6573dbed03d3928dcf4d46c07
AnitaGabor <79316759+AnitaGabor@users.noreply.github.com>
2021-10-26 19:05:07 +0300

    Labor 4

---
+++ QuizApp/app/src/main/java/com/example/quizapp/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView2).apply {
+            text = message
+        }
+    }
+}
